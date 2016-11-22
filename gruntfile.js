27b714ab662a555ae198cb673f91f3ed5bc416a8
Syed Ali <sali3@cox.net>
2016-11-22 11:03:27 -0500

    Worked on build folder, so that the html file shows up in build folder

--- gruntfile.js
+++ gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt){
         {
           expand: true,
           cwd: 'src',
-          src:['index.html'],
+          src:['calc.html'],
           dest: 'build/'
         }
         ]
